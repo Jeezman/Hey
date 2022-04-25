701ab40e60a818c288b993c6c12ee41cea00fd50
Jeezman <tobiadeyemi91@gmail.com>
2022-04-25 12:36:37 +0100

    update icon

--- assets/images/icons.tsx
+++ assets/images/icons.tsx
@@ -56,3 +56,17 @@ export const XIcon = (props: SvgProps) => (
     />
   </Svg>
 )
+
+export const CheckIcon = (props: SvgProps) => (
+  <Svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+    <Path
+      fillRule="evenodd"
+      d="M18.381 5.354a.75.75 0 0 1 .265 1.027l-7.087 12a.75.75 0 0 1-1.164.16L5.48 13.838a.75.75 0 0 1 1.038-1.084l4.23 4.051L17.353 5.62a.75.75 0 0 1 1.027-.265z"
+      clipRule="evenodd"
+    />
+  </Svg>
+)
